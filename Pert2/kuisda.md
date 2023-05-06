489d2412bf2f9a69a47d0a34aa74717e0493548e
root <root@LAPTOP-9AD440EL>
2023-05-06 09:28:12 +0700

    APAJAA

---
+++ Pert2/kuisda.md
@@ -0,0 +1,8 @@
+``` mermaid
+    flowchart TD;
+
+    A(start)--> B[/masukkan angka :]:
+    B--> C{Inputan Angka}
+    C --> I[nilai yang dimasukkan]
+    D --> (finish);
+    ```
---
+++ Pert2/tugas3.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    int beli,bayar,kembalian;
+    cout<<"beli :"<<endl;
+    cin>>beli;
+    cout<<"bayar :"<<endl;
+    cin>>bayar;
+    kembalian = bayar-beli;
+
+    cout<<"hasil kembalian :"<<kembalian<<endl;
+
+    return 0;
+    
+}
